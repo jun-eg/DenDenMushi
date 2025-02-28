9f6056031f639ed93c3f33a90e003a1692d45d50
1F10230014 <s1f102300144@iniad.org>
2025-02-28 16:25:52 +0900

    preline導入

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import formsPlugin from "@tailwindcss/forms";
+import prelinePlugin from "preline/plugin";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "mode_modules/preline/dist/*.js",
   ],
 
   theme: {
@@ -15,6 +18,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [formsPlugin, prelinePlugin],
   darkMode: "selector",
 } satisfies Config;
