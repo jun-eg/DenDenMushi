3b764dd047121b81ee0ba3ee6201e2f3ccf2c1a4
1F10230014 <s1f102300144@iniad.org>
2025-03-04 16:40:02 +0900

    headerの追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Accodion from "./components/Accordion";
+import Header from "./components/Header";
 
 export default async function Home() {
   return (
     <div>
+      <Header />
       <Accodion />
     </div>
   );
