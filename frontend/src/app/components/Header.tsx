5772c76501055b6b447e9e1606daf29821e41a6f
1F10230014 <s1f102300144@iniad.org>
2025-03-07 19:44:56 +0900

    headerコンポ修正

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -2,70 +2,72 @@ import SendTextArea from "./SendTextArea";
 
 const Header = () => {
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-gray-800 text-sm py-3 dark:bg-white">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
-        <div className="flex items-center justify-between">
+    <header className="w-full h-15 bg-gray-800 text-sm py-3 dark:bg-white">
+      <nav className="grid grid-cols-3 w-full h-full px-4 items-center">
+        <div className="text-left">
           <a
-            className="flex-none text-xl font-semibold text-white focus:outline-hidden focus:opacity-80 dark:text-neutral-800"
+            className="text-xl font-semibold text-white focus:outline-hidden focus:opacity-80 dark:text-neutral-800"
             href="#"
           >
             Todo
           </a>
+        </div>
+        <div className="h-full items-center text-center">
           <SendTextArea />
-          <div className="sm:hidden">
-            <button
-              type="button"
-              className="hs-collapse-toggle size-9 flex justify-center items-center gap-2 rounded-lg border border-gray-700 font-medium bg-gray-800 text-gray-400 shadow-2xs align-middle hover:bg-gray-700/20 focus:outline-hidden focus:bg-gray-700/20 text-sm dark:bg-white dark:hover:bg-gray-100 dark:border-gray-200 dark:text-gray-600 dark:focus:bg-gray-100"
-              id="hs-navbar-dark-collapse"
-              aria-expanded="false"
-              aria-controls="hs-navbar-dark"
-              aria-label="Toggle navigation"
-              data-hs-collapse="#hs-navbar-dark"
+        </div>
+        <div className="sm:hidden text-right flex justify-end">
+          <button
+            type="button"
+            className="hs-collapse-toggle size-7 flex justify-center items-center gap-2 rounded-lg border border-gray-700 font-medium bg-gray-800 text-gray-400 shadow-2xs align-middle hover:bg-gray-700/20 focus:outline-hidden focus:bg-gray-700/20 text-sm dark:bg-white dark:hover:bg-gray-100 dark:border-gray-200 dark:text-gray-600 dark:focus:bg-gray-100"
+            id="hs-navbar-dark-collapse"
+            aria-expanded="false"
+            aria-controls="hs-navbar-dark"
+            aria-label="Toggle navigation"
+            data-hs-collapse="#hs-navbar-dark"
+          >
+            <svg
+              className="hs-collapse-open:hidden shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
-              <svg
-                className="hs-collapse-open:hidden shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="3" x2="21" y1="6" y2="6" />
-                <line x1="3" x2="21" y1="12" y2="12" />
-                <line x1="3" x2="21" y1="18" y2="18" />
-              </svg>
-              <svg
-                className="hs-collapse-open:block hidden shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
-              <span className="sr-only">Toggle</span>
-            </button>
-          </div>
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+            <svg
+              className="hs-collapse-open:block hidden shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+            <span className="sr-only">Toggle</span>
+          </button>
         </div>
 
         <div
           id="hs-navbar-dark"
-          className="hidden  hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
+          className="hidden col-start-3 row-start-2 py-2 hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
           aria-labelledby="hs-navbar-dark-collapse"
         >
-          <div className="flex flex-col gap-1 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+          <div className="flex flex-col gap-1 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
             <a
-              className="font-medium text-gray-400 hover:text-gray-500 focus:outline-hidden focus:text-gray-500 dark:text-neutral-500 dark:hover:text-neutral-400 dark:focus:text-neutral-400"
+              className="block text-right font-medium text-gray-400 hover:text-gray-500 focus:outline-hidden focus:text-gray-500 dark:text-neutral-500 dark:hover:text-neutral-400 dark:focus:text-neutral-400"
               href="#"
             >
               ログイン
