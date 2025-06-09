de10013710be739c77d8d7a503ca661ac53b49b5
1F10230014 <s1f102300144@iniad.org>
2025-06-09 17:21:41 +0900

    lintテスト

---
+++ frontend/src/app/components/test.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const Page: FC = () => {
+  return (
+    <main>
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold">Hello World</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
