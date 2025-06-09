f234bae7765364feda51db5100252f06eff27d75
1F10230014 <s1f102300144@iniad.org>
2025-06-09 17:28:57 +0900

    test

--- frontend/src/app/components/test.tsx
+++ frontend/src/app/components/test.tsx
@@ -5,7 +5,8 @@ const Page: FC = () => {
     <main>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">Hello World</h1>
+          
+          
         </div>
       </div>
     </main>
