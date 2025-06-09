28e944a8b2a46acf636a19761285b82ebdb66543
1F10230014 <s1f102300144@iniad.org>
2025-06-09 17:45:00 +0900

    test

---
+++ README.md
@@ -0,0 +1,2 @@
+docker compose run --rm front node -v
+docker compose run --rm api node -v
--- frontend/src/app/components/test.tsx
+++ frontend/src/app/components/test.tsx
@@ -4,10 +4,7 @@ const Page: FC = () => {
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col items-center">
-          
-          
-        </div>
+       
       </div>
     </main>
   );
